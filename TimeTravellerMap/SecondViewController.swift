70811e391a54b53578f924ac25e0a5a36b0b9f11
LingxinGu <lgu8@sheffield.ac.uk>
2016-06-26 09:35:53 +0100

    Initial Commit

---
+++ TimeTravellerMap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TimeTravellerMap
+//
+//  Created by Lingxin Gu on 26/06/2016.
+//  Copyright © 2016 Lingxin Gu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
